790cb566f788c4196ab1b86727589087e35e60a1
AtharvaPaithankar <atharvapaithankar13@gmail.com>
2026-04-06 17:36:24 +0530

    added logi function

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -2,7 +2,9 @@ import express from 'express'
 const router = express.Router()
 
 import TestApp from '../controllers/student/test-app.js'
+import Login from '../controllers/student/login.js'
 
 router.get('/test',TestApp);
+router.post('/login',Login);
 
 export default router;
