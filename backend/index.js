9333c79c48dcb2caadbe249efc0749c44b64173b
Pruthviraj Chougale <pruthvirajchougale17@gmail.com>
2026-08-04 10:19:57 +0530

    Test backend

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.use(cors({
 
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(express.json());
 
 // const ConnectToDB =async () =>{
     
--- backend/routes/student/route.js
+++ backend/routes/student/route.js
@@ -6,7 +6,7 @@ import {addUser} from '../../controllers/student/test-app.js';
 // import { get_tests } from '../controllers/student/test.js';
 // import { protectroute } from '../middlewares/authmiddleware.js';
 
-router.get('/test',addUser);
+router.post('/test',addUser);
 // router.post('/signup', signup);
 // router.post('/login',protectroute, login);
 // router.post('/logout', logout);
